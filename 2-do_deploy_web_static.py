4894e2a6acfbd6cf4ef9e32f1f2c5aad854f4f0e
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-26 18:58:44 +0200

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
 from fabric.api import put, run, env
-from os.path import exists, join
+from os.path import exists
 
-env.hosts = ["100.26.168.126", "54.209.195.60"]
+env.hosts = ["54.209.195.60", "100.26.168.126"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    if not exists(archive_path):
-        print(f"Error: Archive '{archive_path}' does not exist.")
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        
-        run("sudo mkdir -p /data")
-        run("sudo chown -R ubuntu:ubuntu /data")
-        
-        # Upload archive
         put(archive_path, "/tmp/")
-        
-        # Create directory for release
-        run(f"mkdir -p {join(path, no_ext)}")
-        
-        # Extract archive
-        run(f"tar -xzf /tmp/{file_name} -C {join(path, no_ext)}")
-        
-        # Clean up
-        run(f"rm /tmp/{file_name}")
-        
-        # Move files
-        run(f"mv {join(path, no_ext)}/web_static/* {join(path, no_ext)}/")
-        
-        # Remove unnecessary folder
-        run(f"rm -rf {join(path, no_ext)}/web_static")
-        
-        # Update symbolic link
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {join(path, no_ext)}/ /data/web_static/current")
-
-        print("Deployment successful!")
+        run("mkdir -p {}{}/".format(path, no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
+        run("rm /tmp/{}".format(file_n))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+        run("rm -rf {}{}/web_static".format(path, no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
         return True
-    except Exception as e:
-        print(f"Error during deployment: {e}")
+    except:
         return False
