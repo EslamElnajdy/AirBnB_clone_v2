ce3d5381d4d362d443c8cfd804af62ef25dbc7ee
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-12 18:09:02 +0200

    fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,7 +40,7 @@ def number_template_route(n):
     return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
 def number_template(num):
     if num % 2 == 0:
         num_type = 'even'
