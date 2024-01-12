53eb60fd705da6f58b8758ed23511f1cfc963d2c
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-12 02:42:38 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" start a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ return text"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ return text"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
