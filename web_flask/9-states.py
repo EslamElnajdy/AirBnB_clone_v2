5da8b33a05137a721c2f18f7eaa12a63c0a03575
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-13 07:20:53 +0200

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def states_route(state_id=None):
     states = storage.all("State")
     if state_id is not None:
-        state_id = 'state.' + state_id
+        state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
 
 
